20de2724d30ca0db975e6fb97dc67d92538e5fad
Matrixlord <th.nokas@gmail.com>
2018-12-12 13:43:10 +0200

    Updated model

--- data/predict.py
+++ data/predict.py
@@ -8,17 +8,20 @@ import pandas as pd
 # Disable warnings.
 tf.logging.set_verbosity(tf.logging.ERROR)
 
-# Load model.
-model = prediction_model.PredictionModel(modelarg.get_argument())
+def run_prediction():
+    # Load model.
+    model = prediction_model.PredictionModel(modelarg.get_argument())
 
-# Load test data.
-values = np.array(train_data.Data().get_test_photos())
+    # Load test data.
+    values = np.array(train_data.Data().get_test_photos())
 
-len_values = len(values)
-predictions = []
-for w in range(len_values):
-    predictions.append([values[w][1], np.round(model.predict(np.asarray([values[w][0]])) * 100, 2)[0]])
+    len_values = len(values)
+    predictions = []
+    for w in range(len_values):
+        predictions.append([values[w][1], np.round(model.predict(np.asarray([values[w][0]])) * 100, 2)[0]])
 
-df = pd.DataFrame([i[1] for i in predictions], columns=[""], index=[i[0] for i in predictions])
-print("Percentage (%) of Good Leopardism,")
-print(df.groupby(df.index).mean().round(1))
+    df = pd.DataFrame([i[1] for i in predictions], columns=[""], index=[i[0] for i in predictions])
+    print("Percentage (%) of Good Leopardism,")
+    print(df.groupby(df.index).mean().round(1))
+
+run_prediction()
--- data/train.py
+++ data/train.py
@@ -2,6 +2,7 @@ import includes.train_data as train_data
 import includes.prediction_model as prediction_model
 import includes.modelarg as modelarg
 import tensorflow as tf
+from predict import run_prediction 
 
 # Disable warnings.
 tf.logging.set_verbosity(tf.logging.ERROR)
@@ -18,3 +19,6 @@ for i in range(1, training_times + 1):
 
     model.save()
     print('Model saved to disk')
+
+    print('Running prediction now to check progress')
+    run_prediction()
